cc05c43398aeda249c65cd42c03fe6e065c837aa
NathulaDayarathne <nathuladayarathne@gmail.com>
2024-11-05 00:45:59 +0530

    Add Delete method

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -87,6 +87,40 @@ namespace api.Controllers
             }
 
 
+            [HttpDelete]
+            [Route("{id}")]
+
+
+            public IActionResult Delete([FromRoute] int id) 
+            {
+
+                var stockModel =_context.Stocks.FirstOrDefault(x => x.Id == id);
+
+                if(stockModel==null)
+                {
+                    return NotFound();
+                }
+
+                _context.Stocks.Remove(stockModel);
+                _context.SaveChanges();
+
+                return NoContent(); //204
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a3147ee24861ef678030521b63247c72e1e039")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f069cf7b5a094470a5f6b4d4ad17aa24454a63")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
